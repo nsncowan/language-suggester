968a9199fd9179f1b2525cb669a7cf6536c2e3d1
Noah Cowan <nsncowan@gmail.com>
2022-11-05 16:46:57 -0700

    finish HTML and additional javascript conditionals.

--- js/scripts.js
+++ js/scripts.js
@@ -12,28 +12,27 @@ function processSurvey(event) {
   event.preventDefault();
   hideResults();
   const mostImportant = document.getElementById("mostImportant").value;
-  const help = document.querySelector("input[name='revolujtion']:checked").value
-  const name = document.getElementById("userNameInput").value;
+  const question2 = document.querySelector("input[name='question2']:checked").value
+  const name = document.getElementById("userName").value;
   
   document.getElementById("resultsHeading").removeAttribute("class");
   
-  if (mostImportant === "1" && help === "yes") {
+  if (mostImportant === "1" && question2 === "startup") {
     document.getElementById("result1").removeAttribute("class");
     document.querySelector("span#name1").innerText = name;
-  } else if (mostImportant === "2" && help === "yes") {
+  } else if (mostImportant === "2" && question2 === "startup") {
     document.getElementById("result2").removeAttribute("class");
     document.querySelector("span#name2").innerText = name;
-  } else if (mostImportant === "3" && help === "yes") {
+  } else if (mostImportant === "3" && question2 === "startup") {
     document.getElementById("result3").removeAttribute("class");
     document.querySelector("span#name3").innerText = name;
-    document.querySelector("span#name4").innerText = name;
-  } else if (mostImportant === "1" && help === "no") {
+  } else if (mostImportant === "1" && question2 === "established") {
     document.getElementById("result4").removeAttribute("class");
     document.querySelector("span#name5").innerText = name;
-  } else if (mostImportant === "2" && help === "no") {
+  } else if (mostImportant === "2" && question2 === "established") {
     document.getElementById("result5").removeAttribute("class");
     document.querySelector("span#name6").innerText = name;
-  } else if (mostImportant === "3" && help === "no") {
+  } else if (mostImportant === "3" && question2 === "established") {
     document.getElementById("result6").removeAttribute("class");
     document.querySelector("span#name7").innerText = name;
   }
